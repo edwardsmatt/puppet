c33a0dc90504c6f964930f177a3a3ee68c3405f9
Matthew Edwards <matt.e.edwards@gmail.com>
2012-05-19 12:02:08 +1000

    Added ubuntu partner repositories

--- modules/admin/manifests/packages.pp
+++ modules/admin/manifests/packages.pp
@@ -13,27 +13,27 @@ class admin::packages {
   
   class packages {
     $base_packages = [
-                     'openssh-server',
-                     'gnome-shell',
-                     'emacs23-nox',
-                     'puppet-el',
-                     'easytag',
-                     'ack-grep',
-                     'freemind',
-                     'gufw',
-                     'ubuntu-restricted-extras',
-                     'vlc',
-                     'gtk-recordmydesktop',
-                     'git',
-                     'gitk',
-                     'tree',
-                     'cheese',
-                     'cheese-common',
-                     'samba',
-                     'shutter',
-                     'virtualbox',
-                     'ubuntu-tweak',
-                     ]
+                      'openssh-server',
+                      'gnome-shell',
+                      'emacs23-nox',
+                      'puppet-el',
+                      'easytag',
+                      'ack-grep',
+                      'freemind',
+                      'gufw',
+                      'ubuntu-restricted-extras',
+                      'vlc',
+                      'gtk-recordmydesktop',
+                      'git',
+                      'gitk',
+                      'tree',
+                      'cheese',
+                      'cheese-common',
+                      'samba',
+                      'shutter',
+                      'virtualbox',
+                      'ubuntu-tweak',
+                      ]
     
     package { $base_packages:
       ensure => installed,
@@ -41,12 +41,17 @@ class admin::packages {
   }
 
   class repositories {
+
     exec { 'install-virtualbox-repo':
       command  => 'wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | apt-key add -',
     }
     
     exec { 'add-ubuntutweak-ppa':
       command  => 'add-apt-repository ppa:tualatrix/ppa',
+    }
+
+    file { '/etc/apt/sources.list.d/partner.list':
+      content  => "deb http://archive.canonical.com/ubuntu $lsbdistcodename partner\ndeb-src http://archive.canonical.com/ubuntu $lsbdistcodename partner",
     }
   }
 }
